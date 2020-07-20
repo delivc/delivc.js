bd8d4ba7bc65b660d497013975e781d7afd48cce
Julian <me@julian.pro>
2020-07-20 12:05:52 +0200

    chore(package.json): added jest and publishConfig to package.json

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,5 +1,5 @@
 import * as navigation from './navigation'
 
 export default {
-    navigation
+  navigation
 }
--- src/entities/navigation.ts
+++ src/entities/navigation.ts
@@ -1,3 +1,3 @@
-export function wrapNavigation(data: any) {
-    return data
+export function wrapNavigation (data: any) {
+  return data
 }
